58943bf4af4d669e8e784cb7b793bea08122713c
Darren John de Lima <darren.delima@yahoo.co.uk>
2025-11-23 00:14:34 +0000

    Add screen logging and metadata
    Also list last in KUAL

--- blockamazon/bin/block.sh
+++ blockamazon/bin/block.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+col=$((`cat ./bin/COL.txt`))
 mntroot rw
 cp /mnt/us/extensions/blockamazon/hosts/hosts-block /etc/hosts
 mntroot ro
+eips $col 3 "Amazon hosts blocked :)"
--- blockamazon/bin/unblock.sh
+++ blockamazon/bin/unblock.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+col=$((`cat ./bin/COL.txt`))
 mntroot rw
 cp /mnt/us/extensions/blockamazon/hosts/hosts-default /etc/hosts
 mntroot ro
+eips $col 3 "WARNING! Amazon hosts allowed!"
